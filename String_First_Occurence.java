790f5d40f6f39b9e147b1a7c8a7318d5e23f3682
tomar <tomarankit78@gmail.com>
2025-11-12 19:17:55 +0530

    28 28. Find the Leetcode

---
+++ String_First_Occurence.java
@@ -0,0 +1,54 @@
+public class String_First_Occurence {
+    public static void main(String args[]){
+        String haystack="sadwordsad"; int h=haystack.length();
+        String needle="sad";    int n=needle.length();
+
+        // 10 17 9 15 14 19 25 8 4 20 
+        if(n==0){
+            System.out.println("Needle is not present");
+        }
+
+       
+        for(int i=0;i<h-n;i++){
+             boolean match=true;
+
+            for(int j=0;j<n;j++){
+                if(haystack.charAt(i+j)!=needle.charAt(j)){
+                    match=false;
+                    break;
+                }
+            }
+              if(match){
+            System.out.println("Needle is found in haystack");  
+            return;              
+            }
+        }
+          System.out.println(" No needle is in  haystack");    
+          
+
+
+
+    }
+    
+}
+
+/*/
+28. Find the Index of the First Occurrence in a String
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+*/
